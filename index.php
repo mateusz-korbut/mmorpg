728bec48507e9816aa59005c2c54760418bdff45
matixk <matixk73@gmail.com>
2019-03-28 10:42:28 +0100

    Init project

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <title>MMORPG</title>
+
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+<div class="container">
+
+    <header>
+        <h1>MMO users manager</h1>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+        Created by Mateusz Korbut.
+    </footer>
+
+</div>
+</body>
+</html>
