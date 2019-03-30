acb78442223378aa3862da6000c3c1786a0b6460
matixk <matixk73@gmail.com>
2019-03-30 08:41:33 +0100

    Add database connection

---
+++ Configs/databaseConfig.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'host' => '<HOST_NAME>',
+    'user' => '<USERNAME>',
+    'password' => '<PASSWORD>',
+    'database' => '<DATABASE_NAME>'
+];
+$servername = "mysql:host=$host;username:dbname=$dbname";
---
+++ Utils/databaseConnection.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = require_once "../Configs/databaseConfig.php";
+
+$connection = new mysqli($config['host'], $config['user'], $config['password'], $config['database']);
+
+if ($connection->connect_error) {
+    die("Database connection error: " . $connection->connect_error);
+}
+echo "Database connected";
+
+$connection->close();
