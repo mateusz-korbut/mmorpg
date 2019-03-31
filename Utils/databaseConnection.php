33bff152304e6456c6b1157cf7cd908560510e1b
matixk <matixk73@gmail.com>
2019-03-31 12:41:58 +0200

    Add error site

--- Utils/databaseConnection.php
+++ Utils/databaseConnection.php
@@ -5,8 +5,9 @@ $config = include("Configs/databaseConfig.php");
 $connection = new mysqli($config['host'], $config['user'], $config['password'], $config['database']);
 
 if ($connection->connect_error) {
-    die("Database connection error: " . $connection->connect_error);
+    $_SESSION["error"] = $connection->connect_error;
+    header("Location: "."error.php");
+    die();
 }
-echo "Database connected";
 
-$connection->close();
+return $connection;
