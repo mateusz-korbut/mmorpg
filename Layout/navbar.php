b5507cacb327fc218bea969451f3d7809f931ef4
matixk <matixk73@gmail.com>
2019-04-02 14:26:55 +0200

    Move user forms to index.php

--- Layout/navbar.php
+++ Layout/navbar.php
@@ -7,17 +7,6 @@
         <script src="Scripts/auth.js"></script>
         Welcome <span id="username"></span>
         <script>auth.init(<?php echo $_SESSION["user"]; ?>)</script>
-    <?php else: ?>
-        <form id="loginForm">
-            <input type="text" name="name" placeholder="Name">
-            <input type="password" name="password" placeholder="Password">
-            <input type="submit" value="Login">
-        </form>
-        <form id="registerForm">
-            <input type="text" name="name" placeholder="Name" minlength="1">
-            <input type="password" name="password" placeholder="Password" minlength="1">
-            <input type="submit" value="Register">
-        </form>
     <?php endif; ?>
 </header>
 <?php
--- index.php
+++ index.php
@@ -17,7 +17,20 @@
 <div class="container">
 
     <main>
+        <?php if (isset($_SESSION["user"])): ?>
 
+        <?php else: ?>
+            <form id="loginForm">
+                <input type="text" name="name" placeholder="Name">
+                <input type="password" name="password" placeholder="Password">
+                <input type="submit" value="Login">
+            </form>
+            <form id="registerForm">
+                <input type="text" name="name" placeholder="Name" minlength="1">
+                <input type="password" name="password" placeholder="Password" minlength="1">
+                <input type="submit" value="Register">
+            </form>
+        <?php endif; ?>
     </main>
 
     <?php include("Layout/footer.php");?>
