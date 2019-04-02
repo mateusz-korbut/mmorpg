e42a9c3de9f8f0ab493f6a7a6a274a4b283eb230
matixk <matixk73@gmail.com>
2019-04-02 14:23:05 +0200

    Move navbar to layout

--- index.php
+++ index.php
@@ -11,29 +11,13 @@
     <script src="Scripts/index.js" defer></script>
 </head>
 <body>
+
+<?php include("Layout/navbar.php");?>
+
 <div class="container">
 
-    <header>
-        <h1>MMO users manager</h1>
-    </header>
-
     <main>
-        <button id="logoutBtn">Logout</button>
 
-        <?php if (isset($_SESSION["user"])): ?>
-            <?php echo $_SESSION["user"]; ?>
-        <?php else: ?>
-            <form id="loginForm">
-                <input type="text" name="name" placeholder="Name">
-                <input type="password" name="password" placeholder="Password">
-                <input type="submit" value="Login">
-            </form>
-            <form id="registerForm">
-                <input type="text" name="name" placeholder="Name" minlength="1">
-                <input type="password" name="password" placeholder="Password" minlength="1">
-                <input type="submit" value="Register">
-            </form>
-        <?php endif; ?>
     </main>
 
     <?php include("Layout/footer.php");?>
