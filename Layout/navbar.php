e9c6008fdf2d3395ef8a6dd2d284522210b07ad7
matixk <matixk73@gmail.com>
2019-04-03 10:02:02 +0200

    Add links to navbar

--- Layout/navbar.php
+++ Layout/navbar.php
@@ -5,9 +5,13 @@
         <button id="logoutBtn">Logout</button>
 
         <script src="Scripts/auth.js"></script>
-        Welcome <span id="username"></span>
+        Welcome <a href="profile.php"><span id="username"></span></a>
         <script>auth.init(<?php echo $_SESSION["user"]; ?>)</script>
     <?php endif; ?>
+    <a href="leaderboard.php">leaderboard</a>
+    <a href="shop.php">shop</a>
+    <?php if (isset($_SESSION["user"])):
+        include "Utils/isSU.php";?>
+        <a href="manage.php">manage</a>
+    <?php endif; ?>
 </navbar>
-<?php
-
