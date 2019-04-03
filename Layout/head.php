e847e2322603f6ec76e0066260b3dbf0b7f30bf5
matixk <matixk73@gmail.com>
2019-04-03 22:37:54 +0200

    Add bootstrap files

--- Layout/head.php
+++ Layout/head.php
@@ -2,6 +2,8 @@
 <title>MMORPG</title>
 
 <link rel="stylesheet" href="main.css">
+<link rel="stylesheet" href="Styles/bootstrap.min.css">
 <script src="Scripts/jquery-3.3.1.min.js"></script>
+<script src="Scripts/bootstrap.bundle.min.js"></script>
 <script src="Scripts/toaster.js"></script>
 <script src="Scripts/logout.js"></script>
