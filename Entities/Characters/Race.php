9925ab5c5c74e62c3c9732c17c69c8c1180bcad9
matixk <matixk73@gmail.com>
2019-04-03 10:43:22 +0200

    Add enumeration to Race

--- Entities/Characters/Race.php
+++ Entities/Characters/Race.php
@@ -8,6 +8,13 @@ use entities\Entity;
 
 class Race extends Entity
 {
+    const TABLE_NAME = "race";
+
+    const Human = 1;
+    const Elf = 2;
+    const Dwarf = 3;
+    const Orc = 4;
+
     public $strength;
     public $agility;
     public $intelligence;
--- index.php
+++ index.php
@@ -18,22 +18,16 @@
     </header>
 
     <main>
-        <button id="logoutBtn">Logout</button>
-
         <?php if (isset($_SESSION["user"])): ?>
-            <?php echo $_SESSION["user"]; ?>
-        <?php else: ?>
-            <form id="loginForm">
-                <input type="text" name="name" placeholder="Name">
-                <input type="password" name="password" placeholder="Password">
-                <input type="submit" value="Login">
-            </form>
-            <form id="registerForm">
-                <input type="text" name="name" placeholder="Name" minlength="1">
-                <input type="password" name="password" placeholder="Password" minlength="1">
-                <input type="submit" value="Register">
-            </form>
-        <?php endif; ?>
+
+        <?php else:
+            $id = "loginForm";
+            $buttonText = "Login";
+            include "Templates/Index/logregForm.php";
+            $id = "registerForm";
+            $buttonText = "Register";
+            include "Templates/Index/logregForm.php";
+        endif; ?>
     </main>
 
     <?php include("Layout/footer.php");?>
