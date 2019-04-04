541259effa985353251d64807d2cbe23f1403c3f
matixk <matixk73@gmail.com>
2019-04-04 14:09:40 +0200

    Move charts to template

--- index.php
+++ index.php
@@ -22,35 +22,7 @@
     <?php include("Layout/navbar.php");?>
 
     <div class="col-8 container dashboard">
-        <?php if (isset($_SESSION["user"])): ?>
-            <div class="row mt-2 justify-content-center">
-                <div class="col-5">
-                    <div class="card text-center">
-                        <div class="card-body">
-                            <h1 class="card-title">Your characters stats</h1>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row my-4">
-                <div class="col chart">
-                    <div id="levelsChart"></div>
-                </div>
-                <div class="col chart">
-                    <div id="coinsChart"></div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col chart">
-                    <div id="healthChart"></div>
-                </div>
-                <div class="col chart">
-                    <div id="raceChart"></div>
-                </div>
-            </div>
-            <script src="Scripts/chart.js" defer></script>
-            <script type="text/javascript" src="https://canvasjs.com/assets/script/jquery-1.11.1.min.js"></script>
-            <script type="text/javascript" src="https://canvasjs.com/assets/script/jquery.canvasjs.min.js"></script>
+        <?php if (isset($_SESSION["user"])): include_once "Templates/Index/charts.php"?>
 
 <!--            <div class="row">-->
 <!--                Create new character:-->
