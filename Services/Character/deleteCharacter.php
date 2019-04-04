75aa26131c51008f9c5e1f4d54707bec2ce1819b
matixk <matixk73@gmail.com>
2019-04-04 19:09:44 +0200

    Secure actions on character

--- Services/Character/deleteCharacter.php
+++ Services/Character/deleteCharacter.php
@@ -12,23 +12,31 @@ session_start();
 if (isset($_POST["id"]) && isset($_SESSION["user"]))
 {
     $isSu = include_once "Services/Auth/isSu.php";
+    $charCreator = include_once dirname(__FILE__) . "/../Character/getCharacterCreatorId.php";
 
-    $character= new Character();
-    $character->id = mysqli_real_escape_string($connection, $_POST["id"]);
+    if ($charCreator == json_decode($_SESSION["user"])->id || $isSu) {
+        $character= new Character();
+        $character->id = mysqli_real_escape_string($connection, $_POST["id"]);
 
-    $query = sprintf("DELETE FROM %s WHERE characters.id = %d;",
-        Character::TABLE_NAME, $character->id);
+        $query = sprintf("DELETE FROM %s WHERE characters.id = %d;",
+            Character::TABLE_NAME, $character->id);
 
-    if (mysqli_query($connection, $query))
-    {
-        http_response_code(200);
+        if (mysqli_query($connection, $query))
+        {
+            http_response_code(200);
+        }
+        else
+        {
+            echo json_encode(array(
+                "error" => mysqli_error($connection),
+                "query" => $query));
+        }
     }
     else
     {
-        echo json_encode(array(
-            "error" => mysqli_error($connection),
-            "query" => $query));
+        echo json_encode(array("error" => "Must be creator or super user!"));
     }
+
 }
 else
 {
--- Services/Character/editCharacterName.php
+++ Services/Character/editCharacterName.php
@@ -2,8 +2,8 @@
 
 http_response_code(400);
 
-require_once dirname(__FILE__) . "/../Entities/Characters/Character.php";
-require_once dirname(__FILE__) . "/../Utils/databaseConnection.php";
+require_once dirname(__FILE__) . "/../../Entities/Characters/Character.php";
+require_once dirname(__FILE__) . "/../../Utils/databaseConnection.php";
 
 use entities\Characters\Character;
 
@@ -11,22 +11,31 @@ session_start();
 
 if (isset($_POST["name"]) && isset($_POST["id"]) && isset($_SESSION["user"]))
 {
-    $character= new Character();
-    $character->id = mysqli_real_escape_string($connection, $_POST["id"]);
-    $character->name = mysqli_real_escape_string($connection, $_POST["name"]);
+    $isSu = include_once "Services/Auth/isSu.php";
+    $charCreator = include_once dirname(__FILE__) . "/../Character/getCharacterCreatorId.php";
 
-    $query = sprintf("UPDATE %s SET name = '%s' WHERE characters.id = %d;",
-        Character::TABLE_NAME, $character->name, $character->id);
+    if ($charCreator == json_decode($_SESSION["user"])->id || $isSu) {
+        $character= new Character();
+        $character->id = mysqli_real_escape_string($connection, $_POST["id"]);
+        $character->name = mysqli_real_escape_string($connection, $_POST["name"]);
 
-    if (mysqli_query($connection, $query))
-    {
-        http_response_code(200);
+        $query = sprintf("UPDATE %s SET name = '%s' WHERE characters.id = %d;",
+            Character::TABLE_NAME, $character->name, $character->id);
+
+        if (mysqli_query($connection, $query))
+        {
+            http_response_code(200);
+        }
+        else
+        {
+            echo json_encode(array(
+                "error" => mysqli_error($connection),
+                "query" => $query));
+        }
     }
     else
     {
-        echo json_encode(array(
-            "error" => mysqli_error($connection),
-            "query" => $query));
+        echo json_encode(array("error" => "Must be creator or super user!"));
     }
 }
 else
