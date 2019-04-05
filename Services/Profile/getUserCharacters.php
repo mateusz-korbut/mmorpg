da681ab6dc75e669e3929f847e2dd18362b5eb33
matixk <matixk73@gmail.com>
2019-04-05 14:15:27 +0200

    Change logic for showing user characters

--- Services/Profile/getUserCharacters.php
+++ Services/Profile/getUserCharacters.php
@@ -4,17 +4,20 @@ require_once dirname(__FILE__) . "/../../Utils/databaseConnection.php";
 
 $characters = array();
 
-if (isset($_SESSION["user"])) {
-    if (isset($_POST["id"]) && $isSu)
-    {
-        $id = $_POST["id"];
-    }
-    else
-    {
-        $id = json_decode($_SESSION["user"])->id;
-    }
+if (isset($_GET["id"]))
+{
+    $id = $_GET["id"];
+}
+else if (isset($_SESSION["user"]))
+{
+    $id = json_decode($_SESSION["user"])->id;
+}
+else
+{
+    die("You must be logged in or set user id!");
+}
 
-    $query = sprintf("SELECT
+$query = sprintf("SELECT
                                 characters.id,
                                 characters.name,
                                 characters.level,
@@ -25,18 +28,15 @@ if (isset($_SESSION["user"])) {
                             ON race_id = race.id
                             WHERE creator_id = %d;", $id);
 
-    $result = $connection->query($query);
+$result = $connection->query($query);
 
-    if ($result) {
-        while($row = $result->fetch_object()){
-            array_push($characters, $row);
-        }
+if ($result) {
+    while($row = $result->fetch_object()){
+        array_push($characters, $row);
     }
-    else {
-        die($connection->error);
-    }
-
-    return $characters;
-} else {
-    die("Unable to download data. User not logged in!");
 }
+else {
+    die($connection->error);
+}
+
+return $characters;
