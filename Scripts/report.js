401994088fe49178baf01e3cd4617b90b4c8cbf6
Matixk <matixk73@gmail.com>
2019-06-02 15:11:58 +0200

    Add js script which display line chart on report site

---
+++ Scripts/report.js
@@ -0,0 +1,35 @@
+let report = {
+    init: function () {
+        this.displayReport();
+    },
+    displayReport: function () {
+        $.get("Services/getDailyStats.php", function (data) {
+            toaster.show(data);
+            data = JSON.parse(data);
+            let login = [];
+            let register = [];
+
+            data.forEach(e => {
+                login.push({x: report.toDate(e.date), y: parseInt(e.logged_in)});
+                register.push({x: report.toDate(e.date), y: parseInt(e.registered)});
+            });
+
+            console.log(login);
+            console.log(register);
+
+            // chart.init("Logged in", login, "#loginChart");
+            // chart.init("Registered", register, "#registerChart");
+            chart.series(register, login);
+        })
+            .fail(function(data) {
+                toaster.show(data);
+            });
+    },
+    toDate(string) {
+        return new Date(string.replace( /(\d{2})-(\d{2})-(\d{4})/, "$2/$1/$3"));
+    }
+};
+
+$(document).ready(function() {
+    report.init();
+});
